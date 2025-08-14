b520d6461c8bcea2aa6c6bf3a58b678f224a0d32
sato96 <samtozzi.st@gmail.com>
2025-08-15 00:01:31 +0200

    version 0.1.3

--- cli_loading_bar/__init__.py
+++ cli_loading_bar/__init__.py
@@ -1,7 +1,8 @@
 from .Loading import *  # importa tutte le classi, funzioni, variabili definite in Loading.py
-
+import importlib.metadata
 # opzionale: limita ciò che viene esposto con __all__
 # Se non lo metti, verrà esposto tutto ciò che non inizia con "_"
 from .Loading import __all__ as loading_all
 __all__ = loading_all
 
+__version__ = importlib.metadata.version("cli_loading_bar")
